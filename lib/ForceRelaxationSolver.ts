a30cfdacdc37184f872c5fcba786795af38fed5c
seveibar <seveibar@gmail.com>
2026-01-04 11:26:53 -0800

    wip

--- lib/ForceRelaxationSolver.ts
+++ lib/ForceRelaxationSolver.ts
@@ -1,6 +1,17 @@
 import { BaseSolver } from "@tscircuit/solver-utils"
 import type { GraphicsObject } from "graphics-debug"
+import type { ForceRelaxationProblem } from "./types"
+
 export class ForceRelaxationSolver extends BaseSolver {
+  constructor(public input: ForceRelaxationProblem) {
+    super()
+    this.MAX_ITERATIONS = input.solve.maxSteps
+  }
+
+  override _step(): void {
+    // TODO perform forces
+  }
+
   override visualize(): GraphicsObject {
     const graphics = {
       points: [],
@@ -12,6 +23,8 @@ export class ForceRelaxationSolver extends BaseSolver {
       coordinateSystem: "cartesian",
       title: "force relaxation solver",
     } as Required<GraphicsObject>
+
+    // TODO Draw points and segments
 
     return graphics
   }
